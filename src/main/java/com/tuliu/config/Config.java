5a06652c5b3ab3f305d696c10aa50d2ae19fc275
liuzhenzhen <761501587@qq.com>
2017-03-02 15:22:11 +0800

    test git java

---
+++ src/main/java/com/tuliu/config/Config.java
@@ -0,0 +1,15 @@
+package com.tuliu.config;
+
+/**
+ * ---- 请加注释 ------
+ * 
+ * @Author liuzhenzhen
+ * @CreateTime 2017年3月2日 下午3:15:08
+ */
+public class Config {
+    public static void main (String[] args) {
+        System.out.println ("#################3");
+        System.out.println ("java config!!!!!!");
+    }
+
+}
